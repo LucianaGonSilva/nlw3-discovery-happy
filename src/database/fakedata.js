8ca76e78646c928e3125935a974572d10daee360
Luciana Gonçalves <luciana.cns@hotmail.com>
2020-10-24 10:53:54 -0300

    Workshop nlw#3 completo

---
+++ src/database/fakedata.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633",
+        lng: "-49.6455874",
+        name: "Lar das meninas",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "(16)98100-6307",
+        images: [
+            "https://images.unsplash.com/photo-1573486145949-182147241fa6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1590966473477-e74b95a0c407?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha se sentir à vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas das 08h até as 18h",
+        open_on_weekends: "1",
+
+    },
+
+    {
+        id: 2,
+        lat: "-27.222633",
+        lng: "-49.6555874",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1573486145949-182147241fa6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1590966473477-e74b95a0c407?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha se sentir à vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas das 08h até as 18h",
+        open_on_weekends: "0",
+    },
+]
---
+++ src/database/test.js
@@ -0,0 +1,33 @@
+const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage');
+
+Database.then(async db => {
+  // inserir dados na tabela
+await saveOrphanage(db, {
+    lat: "-27.222633",
+    lng: "-49.6555874",
+    name: "Lar dos meninos",
+    about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    whatsapp: "(16) 98100-6307",
+    images: [
+      "https://images.unsplash.com/photo-1573486145949-182147241fa6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1590966473477-e74b95a0c407?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ].toString(),
+    instructions: "Venha se sentir à vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horários de visitas das 08h até as 18h",
+    open_on_weekends: "0"
+  }) 
+
+  // consultar dados da tabela
+  const selectedOrphanages = await db.all("SELECT * FROM orphanages")
+  console.log(selectedOrphanages)
+
+  // consultar somente um orfanato pelo id
+  const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
+  console.log(orphanage)
+
+  // deletar dados da tabela
+  // console.log (await db.run("DELETE FROM orphanages WHERE id = '4'"))
+  // console.log (await db.run("DELETE FROM orphanages WHERE id = '5'"))
+})
